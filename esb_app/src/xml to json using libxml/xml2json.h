3dd052597da43da427bf787652cb0111d1ca0f96
riyatoteja1998 <47156108+riyatoteja1998@users.noreply.github.com>
2020-09-08 18:58:23 +0530

    Create xml2json.h

---
+++ esb_app/src/xml to json using libxml/xml2json.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+
+/* 
+takes in xml file and uses libxml to convert it in a tree like data structure and then prints in json format using libxml.
+*/
+ 
+void xml_to_json(char* xml_file, char* json_file);
