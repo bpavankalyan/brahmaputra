741dc6e90a4ff55e37eecd8a8202179631dbc012
riyatoteja1998 <47156108+riyatoteja1998@users.noreply.github.com>
2020-09-08 13:07:59 +0530

    Update test_xml2json.c

--- esb_app/src/xml to json using libxml/test_xml2json.c
+++ esb_app/src/xml to json using libxml/test_xml2json.c
@@ -1,5 +1,7 @@
 #include <libxml/tree.h>
 
+#include <stdio.h>
+
 #include <sys/types.h>
 
 #include <unistd.h>
@@ -8,12 +10,10 @@
 
 #include "xml2json.c"
 
-int main() {
-  char * input = "bmd.xml";
-  char * output = "bmd2.json";
-  bmd * xyz;
-  xyz = xml_to_json(input, output);
-
+int main(){
+  char* input="bmd.xml";
+  char * output="bmd2.json";
+  printf("%s generated successfully", output);
+  xml_to_json(input, output);
   return 0;
 
-}
