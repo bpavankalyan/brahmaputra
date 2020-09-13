7cd06b6c831d1261c655cd219c7a1660dc87ff86
riyatoteja1998 <47156108+riyatoteja1998@users.noreply.github.com>
2020-09-13 14:39:13 +0000

    Update esb_app.c

--- esb_app/src/esb_app.c
+++ esb_app/src/esb_app.c
@@ -83,6 +83,27 @@ int esb_endpoint(struct http_request *req)
 	}
 }
 
+
+static void _mkdir(const char *dir) {
+        char tmp[256];
+        char *p = NULL;
+        size_t len;
+
+        snprintf(tmp, sizeof(tmp),"%s",dir);
+        len = strlen(tmp);
+        if(tmp[len - 1] == '/')
+                tmp[len - 1] = 0;
+        for(p = tmp + 1; *p; p++)
+                if(*p == '/') {
+                        *p = 0;
+                        mkdir(tmp, S_IRWXU);
+                        *p = '/';
+                }
+        mkdir(tmp, S_IRWXU);
+
+}
+
+  
 char *create_work_dir_for_request()
 {
 	kore_log(LOG_INFO, "Creating the temporary work folder.");
@@ -90,10 +111,16 @@ char *create_work_dir_for_request()
 	 * TODO: Create a temporary folder in the current directory.
 	 * Its name should be unique to each request.
 	 */
-	char *temp_path = malloc(PATH_MAX * sizeof(char));
-	strcpy(temp_path, "./bmd_files/1234");
+        char *temp_path = malloc(PATH_MAX * sizeof(char));
+	time_t now = time(NULL);
+	srand(now);
+	char cwd[100];
+	getcwd(cwd,sizeof(cwd));
+	sprintf(temp_path,"%s/bmd_files/%ld",cwd,now);
+	_mkdir(temp_path);
 	kore_log(LOG_INFO, "Temporary work folder: %s", temp_path);
-	return temp_path;
+        return temp_path;
+	
 }
 
 endpoint_result
