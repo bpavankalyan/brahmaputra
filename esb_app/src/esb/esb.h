d86c7a7478cd4e9d03b39881cf1f383985365e23
riyatoteja1998 <47156108+riyatoteja1998@users.noreply.github.com>
2020-09-13 14:46:55 +0000

    Update esb.h

--- esb_app/src/esb/esb.h
+++ esb_app/src/esb/esb.h
@@ -1,16 +1,14 @@
 int process_esb_request(char* bmd_file_path);
 
 typedef struct {
-    
-    char* MessageID;
-    char* MessageType; 
-    char* Sender; 
+    char* Sender;
     char* Destination;
-    char* CreationDateTime;
-    char* Signature;
-    char* ReferenceID;
-    char* Payload;
-    // TODO: Other fields
+    char* MessageType;
+    char * MessageID;
+    char * CreationDateTime;
+    char * Signature;
+    char * ReferenceID;
+
 } bmd_envelop;
 
 typedef struct {
