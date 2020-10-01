2cbd58697b9d303cccb98a352d6b9f8e330279b7
bpavankalyan <46530943+bpavankalyan@users.noreply.github.com>
2020-10-01 08:45:41 +0530

    Update input.sh

--- esb_app/src/Testcases/input.sh
+++ esb_app/src/Testcases/input.sh
@@ -4,7 +4,7 @@
 if [ "$1" = "bmd" ]; then
 	while [ -n "$2" ]; do
 		echo "$2"
-		curl --insecure -F "bmd_file=@"$2"" https://localhost:8888/bmd
+		curl --insecure -F "bmd_file=@$2" https://localhost:8888/bmd
 		shift
 	done
 elif [ "$1" = "get-status" ]; then
